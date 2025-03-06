325f63d5d9798ee6d691edbc598f4bc96ca6e974
Anton Nevezhin <anton-nevezhin@yandex.ru>
2025-03-06 13:15:03 +0300

    First corrected

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 let newdiv = document.createElement('div');
 document.body.appendChild(newdiv)
-//newdiv.style.marginLeft = 'auto';
-//newdiv.style.marginRight = '100px';
 newdiv.setAttribute('style', 'border: 1px solid blue; width: 200px; height: 200px; backgroundColor: red; position: fixed; right: 100px; bottom: 100px; ')
-//newdiv.style.width = '200px';
-//newdiv.style.height = '200px';
-//newdiv.style.position = 'fixed';
-//newdiv.style.right = '100px';
-//newdiv.style.bottom = '100px';
 newdiv.style.backgroundColor = 'red';
