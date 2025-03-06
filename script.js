356cc80cdf0db141939d3843932e006b50157ca8
Anton Nevezhin <anton-nevezhin@yandex.ru>
2025-03-06 12:36:41 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let newdiv = document.createElement('div');
+document.body.appendChild(newdiv)
+newdiv.style.backgroundColor = 'red';
+//newdiv.style.marginLeft = 'auto';
+//newdiv.style.marginRight = '100px';
+newdiv.style.width = '200px';
+newdiv.style.height = '200px';
+newdiv.style.position = 'fixed';
+newdiv.style.right = '100px';
+newdiv.style.bottom = '100px';
