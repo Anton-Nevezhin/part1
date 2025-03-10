df02cbc38d92591866de883442379211fc12f0a2
Anton Nevezhin <anton-nevezhin@yandex.ru>
2025-03-10 15:09:35 +0300

    Add file errors

--- script.js
+++
@@ -1,4 +0,0 @@
-let newdiv = document.createElement('div');
-document.body.appendChild(newdiv)
-newdiv.setAttribute('style', 'border: 1px solid blue; width: 200px; height: 200px; backgroundColor: red; position: fixed; right: 100px; bottom: 100px; ')
-newdiv.style.backgroundColor = 'red';
